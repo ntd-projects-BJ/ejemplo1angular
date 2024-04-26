b6a2fa6e29e2e9c3841c8101823a3e4f960a447c
SebastianSamanieg <Sebastian_Samaniego@outlook.com>
2024-04-25 20:29:27 -0500

    Configuracion de proxy, creacion de servivio y componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AnimalComponent } from './components/animal/animal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'animals', component: AnimalComponent},
+];
